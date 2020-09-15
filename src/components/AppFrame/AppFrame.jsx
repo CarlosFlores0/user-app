fe5c9dfa0b2a542c67b6a3f727ef925c01d9da8a
CarlosFlores0 <carlosfc.dev@gmail.com>
2020-09-14 23:34:07 -0500

    Preparando base del proyecto

---
+++ src/components/AppFrame/AppFrame.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AppFrame = () => {
+  return (
+    <div>
+      AppFrame
+    </div>
+  )
+}
+
+export default AppFrame
+AppFrame
---
+++ src/components/UserList/UserList.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const UserList = props => {
+  return (
+    <div>
+      UserList
+    </div>
+  )
+}
+
+UserList.propTypes = {
+
+}
+
+export default UserList
