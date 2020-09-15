82dfbbbe47d5876990953bafe010374cb87d4e6c
CarlosFlores0 <carlosfc.dev@gmail.com>
2020-09-15 11:46:16 -0500

    Se limpio la aplicación y se colocaron los pages

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router,
+  Switch,
+  Route } from 'react-router-dom'
+import MainPage from './pages/MainPage'
+import NotFoundPage from './pages/NotFoundPage'
+import UserPage from './pages/UserPage'
+import WelcomePage from './pages/WelcomePage'
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <WelcomePage/>
+        </Route>
+        <Route path="/main">
+          <MainPage/>
+        </Route>
+        <Route path="/user">
+          <UserPage/>
+        </Route>
+        <Route>
+          <NotFoundPage/>
+        </Route>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
