a8cfbf6fa39e6437baf229fbed41688e0a820982
CarlosFlores0 <carlosfc.dev@gmail.com>
2020-09-15 15:46:38 -0500

    Componente AppFrame terminado

--- src/components/AppFrame/AppFrame.jsx
+++ src/components/AppFrame/AppFrame.jsx
@@ -1,11 +1,60 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar'
+import Grid from '@material-ui/core/Grid'
+import Link from '@material-ui/core/Link'
+import { Link as LinkRouter } from 'react-router-dom'
+import Typography from '@material-ui/core/Typography'
+import Toolbar from '@material-ui/core/Toolbar'
 
-const AppFrame = () => {
+const useStyles = makeStyles((theme) => ({
+  about: {
+    marginRight: theme.spacing(4),
+  },
+}));
+
+const AppFrame = ({ children }) => {
+  const classes = useStyles();
   return (
-    <div>
-      AppFrame
-    </div>
+    <Grid container
+      justify="center">
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Link
+              className={classes.about}
+              component={LinkRouter}  
+              to="/main" 
+              color="inherit" 
+              aria-label="menu">
+              <Typography variant="h6" color="inherit">
+                Acerca del proyecto
+              </Typography>
+            </Link>
+            <Link
+              component={LinkRouter}  
+              to="/main" 
+              color="inherit" 
+              aria-label="menu">
+              <Typography variant="h6" color="inherit">
+                Lista de usuarios
+              </Typography>
+            </Link>
+          </Toolbar>
+        </AppBar>
+        <Grid item
+          xs={12}
+          sm={11}
+          md={10}
+          lg={8}>
+            {children}
+        </Grid>
+    </Grid>
   )
+}
+
+AppFrame.propTypes = {
+  children: PropTypes.node
 }
 
 export default AppFrame
--- src/components/AppFrame/AppFrame.stories.jsx
+++ src/components/AppFrame/AppFrame.stories.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import AppFrame from './AppFrame'
+import { BrowserRouter as Router} from 'react-router-dom'
 
 export default {
   title: "AppFrame",
   component: AppFrame
 }
 
-export const AppFrameExample = () => (<AppFrame/>)
+export const AppFrameExample = () => (
+  <Router>
+    <AppFrame>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quidem fugit doloremque fuga. Maxime asperiores repellendus dolore aliquam saepe dolorum nesciunt atque non enim magni, totam, maiores voluptate ex harum.
+    </AppFrame>
+  </Router>
+)
