e2c18324f4edf302dcb6c529ed86dda36aa72f38
CarlosFlores0 <carlosfc.dev@gmail.com>
2020-09-16 22:48:35 -0500

    Bug de hooks arreglado

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -10,12 +10,18 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { Grid } from '@material-ui/core';
 
-// const users = [
-//   { id: 0, name: "Carlos Flores", email: "carlosfc.dev@gmail.com", address: { city: "Monterrey"}, phone: "52-8121757767", website: "carlosflores.com", company: {name:"Company"}},
-//   { id: 1, name: "Jose Lopez", email: "Lopez.dev@gmail.com", address: { city: "Ciudad de México"}, phone: "52-8184220121", website: "jose-lopez.com", company: {name:"Company 2"}},
-//   { id: 2, name: "Jorge Garza", email: "Jorge.pollo@gmail.com", address: { city: "Guadalajara"}, phone: "52-8150339204", website: "jorge-pollo.com", company: {name:"Company 3"}},
-//   { id: 3, name: "Edith Garza", email: "EdyGarza@gmail.com", address: { city: "Monterrey"}, phone: "52-8199402931", website: "edy-garza.com", company: {name:"Company 4"}},  
-// ]
+const users = [
+  { id: 0, name: "Carlos Flores", email: "carlosfc.dev@gmail.com", address: { city: "Monterrey"}, phone: "52-8121757767", website: "carlosflores.com", company: {name:"Company"}},
+  { id: 1, name: "Jose Lopez", email: "Lopez.dev@gmail.com", address: { city: "Ciudad de México"}, phone: "52-8184220121", website: "jose-lopez.com", company: {name:"Company 2"}},
+  { id: 2, name: "Jorge Garza", email: "Jorge.pollo@gmail.com", address: { city: "Guadalajara"}, phone: "52-8150339204", website: "jorge-pollo.com", company: {name:"Company 3"}},
+  { id: 3, name: "Edith Garza", email: "EdyGarza@gmail.com", address: { city: "Monterrey"}, phone: "52-8199402931", website: "edy-garza.com", company: {name:"Company 4"}},  
+  { id: 4, name: "Carlos Flores", email: "carlosfc.dev@gmail.com", address: { city: "Monterrey"}, phone: "52-8121757767", website: "carlosflores.com", company: {name:"Company"}},
+  { id: 5, name: "Jose Lopez", email: "Lopez.dev@gmail.com", address: { city: "Ciudad de México"}, phone: "52-8184220121", website: "jose-lopez.com", company: {name:"Company 2"}},
+  { id: 6, name: "Jorge Garza", email: "Jorge.pollo@gmail.com", address: { city: "Guadalajara"}, phone: "52-8150339204", website: "jorge-pollo.com", company: {name:"Company 3"}},
+  { id: 8, name: "Edith Garza", email: "EdyGarza@gmail.com", address: { city: "Monterrey"}, phone: "52-8199402931", website: "edy-garza.com", company: {name:"Company 4"}},  
+  { id: 9, name: "Jose Lopez", email: "Lopez.dev@gmail.com", address: { city: "Ciudad de México"}, phone: "52-8184220121", website: "jose-lopez.com", company: {name:"Company 2"}},
+  { id: 10, name: "Jorge Garza", email: "Jorge.pollo@gmail.com", address: { city: "Guadalajara"}, phone: "52-8150339204", website: "jorge-pollo.com", company: {name:"Company 3"}},
+]
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -40,10 +46,10 @@ const useStyles = makeStyles({
 
 const renderUser = eventOnClickUser => user => {
   const { id, name, email, address, phone, website, company } = user
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <TableRow className={classes.cell} hover role="checkbox" key={id} onClick={eventOnClickUser}>
+    <TableRow className="cell-table" hover role="checkbox" key={id} onClick={eventOnClickUser}>
       <StyledTableCell component="th" scope="row">
         {id}
       </StyledTableCell>
@@ -71,31 +77,29 @@ const UserList = () => {
     const url = "https://jsonplaceholder.typicode.com/users"
 
     const setUser = async () => {
-      if (allUser.users.length === 0){
-        try {
-          let response = await fetch(url)
-          let responseJson = await response.json()
-          
-          setAllUser({users:responseJson})
-        } catch (error) {      
-          if (error.response) { // Errores que nos responde el server
-            console.log("Ha ocurrido un error en el servidor")
-          } else if (error.request) { // Errores que suceden por no llegar al server
-            console.log("Verifique la conexión a internet")
-          } else { // Errores imprevistos
-            console.log("Error al cargar los datos")
-          }  
-        }
+      try {
+        let response = await fetch(url)
+        let responseJson = await response.json()
+        
+        setAllUser({users:responseJson})
+      } catch (error) {      
+        if (error.response) { // Errores que nos responde el server
+          console.log("Ha ocurrido un error en el servidor")
+        } else if (error.request) { // Errores que suceden por no llegar al server
+          console.log("Verifique la conexión a internet")
+        } else { // Errores imprevistos
+          console.log("Error al cargar los datos")
+        }  
       }
     }
     setUser();
-  },[allUser])
+  },[])
 
   return (
     <Grid container
       justify="center">
       {
-        allUser.users ?
+        users ?
         <TableContainer 
         className={classes.table} 
         component={Paper}
