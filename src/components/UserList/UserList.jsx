e1b0f5c00c469ebc253d1e14dada5c8c01de2294
CarlosFlores0 <carlosfc.dev@gmail.com>
2020-09-16 19:39:35 -0500

    Se intento actualizar la tabla atraves de hooks

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -10,12 +10,12 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import { Grid } from '@material-ui/core';
 
-const users = [
-  { id: 0, name: "Carlos Flores", email: "carlosfc.dev@gmail.com", address: { city: "Monterrey"}, phone: "52-8121757767", website: "carlosflores.com", company: {name:"Company"}},
-  { id: 1, name: "Jose Lopez", email: "Lopez.dev@gmail.com", address: { city: "Ciudad de México"}, phone: "52-8184220121", website: "jose-lopez.com", company: {name:"Company 2"}},
-  { id: 2, name: "Jorge Garza", email: "Jorge.pollo@gmail.com", address: { city: "Guadalajara"}, phone: "52-8150339204", website: "jorge-pollo.com", company: {name:"Company 3"}},
-  { id: 3, name: "Edith Garza", email: "EdyGarza@gmail.com", address: { city: "Monterrey"}, phone: "52-8199402931", website: "edy-garza.com", company: {name:"Company 4"}},  
-]
+// const users = [
+//   { id: 0, name: "Carlos Flores", email: "carlosfc.dev@gmail.com", address: { city: "Monterrey"}, phone: "52-8121757767", website: "carlosflores.com", company: {name:"Company"}},
+//   { id: 1, name: "Jose Lopez", email: "Lopez.dev@gmail.com", address: { city: "Ciudad de México"}, phone: "52-8184220121", website: "jose-lopez.com", company: {name:"Company 2"}},
+//   { id: 2, name: "Jorge Garza", email: "Jorge.pollo@gmail.com", address: { city: "Guadalajara"}, phone: "52-8150339204", website: "jorge-pollo.com", company: {name:"Company 3"}},
+//   { id: 3, name: "Edith Garza", email: "EdyGarza@gmail.com", address: { city: "Monterrey"}, phone: "52-8199402931", website: "edy-garza.com", company: {name:"Company 4"}},  
+// ]
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -65,11 +65,38 @@ const UserList = () => {
     history.push("/user")
   }
 
+  const [allUser, setAllUser] = useState({users:[]})
+
+  useEffect(() => {
+    const url = "https://jsonplaceholder.typicode.com/users"
+
+    const setUser = async () => {
+      if (allUser.users.length === 0){
+        try {
+          let response = await fetch(url)
+          let responseJson = await response.json()
+          
+          setAllUser({users:responseJson})
+        } catch (error) {      
+          if (error.response) { // Errores que nos responde el server
+            console.log("Ha ocurrido un error en el servidor")
+          } else if (error.request) { // Errores que suceden por no llegar al server
+            console.log("Verifique la conexión a internet")
+          } else { // Errores imprevistos
+            console.log("Error al cargar los datos")
+          }  
+        }
+      }
+    }
+    setUser();
+  },[allUser])
 
   return (
     <Grid container
       justify="center">
-      <TableContainer 
+      {
+        allUser.users ?
+        <TableContainer 
         className={classes.table} 
         component={Paper}
         elevation={3}>
@@ -86,10 +113,15 @@ const UserList = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {users.map((user) => renderUser(onClickHandler)(user))}
+            {allUser.users.map((user) => renderUser(onClickHandler)(user))}
           </TableBody>
         </Table>
       </TableContainer>
+      :
+      <Grid item>
+        <h2>Nothing</h2>
+      </Grid>
+      }
     </Grid>
   )
 }
